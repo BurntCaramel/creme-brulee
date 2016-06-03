bbbc7884951ebb3d9c901a9db0e26248bf9a28ce
Patrick Smith <pgwsmith@gmail.com>
2016-06-03 23:24:38 +1000

    Fix return value of joiPromise

--- src/utils/joiPromise.js
+++ src/utils/joiPromise.js
@@ -1,9 +1,10 @@
+const R = require('ramda')
 const Joi = require('joi')
 
 module.exports = (schema) => (value) => (
 	Promise.resolve(value)
-	.then(value => Joi.assert(
+	.then(R.tap((value) => Joi.assert(
 		value,
 		Joi.compile(schema)
-	))
+	)))
 )
