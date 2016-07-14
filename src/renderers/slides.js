eb4c8d059de2846bf188ac541e78fc5f3c13070e
Patrick Smith <pgwsmith@gmail.com>
2016-07-14 22:11:04 +1000

    Add fonts and prism code highlighting to slides renderer

--- src/renderers/slides.js
+++ src/renderers/slides.js
@@ -11,7 +11,14 @@ module.exports = (options) => R.tryCatch(
 <div id="json" style="display: none">${ input }</div>
 <div id="app"></div>
 `,
+				headElements: [
+					`<link href="https://fonts.googleapis.com/css?family=Lobster+Two:400,700" rel="stylesheet" type="text/css">`,
+					`<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet" type="text/css">`,
+					`<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/prism/1.3.0/themes/prism-tomorrow.css">`
+				],
 				bodyLastElements: [
+					`<script src="https://cdn.jsdelivr.net/prism/1.3.0/prism.js" type="text/javascript"></script>`,
+					`<script src="https://cdn.jsdelivr.net/prism/1.3.0/components/prism-jsx.min.js" type="text/javascript"></script>`,
 					`<script src="/1/-web/slides/vendor.js"></script>`,
 					`<script src="/1/-web/slides/app.js"></script>`
 				]
