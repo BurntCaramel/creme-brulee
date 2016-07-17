2415138d49d3371eb95037ae223c195ee14ea000
Patrick Smith <pgwsmith@gmail.com>
2016-07-17 15:22:30 +1000

    Update royal-piping, allowing action lists
    Also allows error messages via Boom when rendering

--- src/renderers/piping.js
+++ src/renderers/piping.js
@@ -2,15 +2,42 @@ const R = require('ramda')
 const Boom = require('boom')
 const { createElement } = require('react')
 const { renderToStaticMarkup } = require('react-dom/server')
-const RoyalPiping = require('royal-piping')
+const { Screen, ActionList } = require('royal-piping')
+const kit = require('royal-piping/lib/kits/ios')
+const { makeMap: makeDevicesMap } = require('royal-piping/lib/devices/apple')
 
-module.exports = (options) => (content) => (
-	R.merge(options, {
-		innerHTML: renderToStaticMarkup(
-			createElement(
-				RoyalPiping,
-				{ content: [].concat(content) }
+const devicesMap = makeDevicesMap()
+
+module.exports = (options) => R.tryCatch(
+	({ type, content, deviceID = 'iphone6' }) => (
+		R.merge(options, {
+			innerHTML: renderToStaticMarkup(
+				createElement(
+					R.cond([
+						[
+							R.equals('screen'),
+							R.always(Screen)
+						],
+						[
+							R.equals('actionList'),
+							R.always(ActionList)
+						],
+						[
+							R.T,
+							() => { throw `Unknown type: '${type}'` }
+						]
+					])(type),
+					{
+						content: [].concat(content), // Ensure array
+						kit,
+						deviceInfo: devicesMap.get(deviceID)
+					}
+				)
 			)
-		)
-	})
+		})
+	),
+	(error) => {
+		console.error(error)
+		throw Boom.wrap(error, 406, 'Incorrect format for Royal Piping')
+	}
 )
