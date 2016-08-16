4029d7068fe7b9a80b7564899f2399a0ecfb367b
Patrick Smith <pgwsmith@gmail.com>
2016-08-16 23:47:49 +1000

    Stub benchtop page

--- src/sections/benchtop/index.js
+++ src/sections/benchtop/index.js
@@ -1,5 +1,27 @@
+const R = require('ramda')
+const { createElement } = require('react')
+//const FondantEditor = require('fondant-react')
+//const { base16ToEmoji } = require('emojid')
+
+const renderElement = require('../../templates/react')
+
 exports.routes = [
 	{
-		path: `benchtop` // marble?
+		method: 'GET',
+		path: `/benchtop/@{organization}/sha256:{sha256}`, // marble?
+		handler({ params: { organization, sha256 }}, reply) {
+			reply('Coming soon')
+			/*reply(
+				renderTemplate({
+					title: `Edit recipe`,
+					innerHTML: renderElement({
+						title: `Editing recipe from @{organization}`,
+						element: createElement(FondantEditor, {
+							initialTransforms: []
+						})
+					})
+				})
+			)*/
+		}
 	}
 ]
--- src/sections/index.js
+++ src/sections/index.js
@@ -3,7 +3,7 @@ const R = require('ramda')
 const communication = require('./communication')
 //const examples = require('./examples')
 //const showcase = require('./showcase')
-//const benchtop = require('./benchtop')
+const benchtop = require('./benchtop')
 //const pantry = require('./pantry')
 //const account = require('./account')
 
@@ -11,7 +11,7 @@ exports.routes = R.unnest([
 	communication.routes,
 	//examples.routes,
 	//showcase.routes,
-	//benchtop.routes,
+	benchtop.routes,
 	//pantry.routes,
 	//account.routes
 ])
